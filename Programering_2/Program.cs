e2fc7fad34251fa5b090d170aa7a680c2e481224
Aleksandar_Hemmadator <mrdjen.aleksandar@elev.ga.ntig.se>
2022-09-17 13:11:42 +0200

    First

---
+++ Programering_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Ball{
+
+public int x;
+
+public int y;
+
+// för att kunna få r i rgb så måste man skriva rgb.r
+public (int r, int g, int b) rgb;
+
+//r för radie
+public int r;
+
+}
